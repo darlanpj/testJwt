9dd04b346826cca104071d119a72b7b292d13e4a
djambersi <djambersi@uolinc.com>
2024-07-14 22:22:17 -0300

    refactoring

--- src/main/java/org/example/testjwt/service/JwtValidation.java
+++ src/main/java/org/example/testjwt/service/JwtValidation.java
@@ -5,6 +5,7 @@ import com.auth0.jwt.interfaces.DecodedJWT;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.example.testjwt.model.JwtPayload;
+import org.example.testjwt.service.impl.ValidationRules;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -13,7 +14,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
-public class JwtValidation {
+public class JwtValidation implements ValidationRules {
 
     private static final Log LOG = LogFactory.getLog(JwtValidation.class);
     public boolean validateJwtToken(String token) {
@@ -38,28 +39,28 @@ public class JwtValidation {
         }
     }
 
-    private boolean validadeClaimsName(JwtPayload jwtPayload) {
+    public boolean validadeClaimsName(JwtPayload jwtPayload) {
         Collection<String> keys = Set.of("Role", "Seed", "Name");
         return keys.containsAll(jwtPayload.getDetails().keySet());
     }
 
-    private boolean validateClaimNameIsNumeric(JwtPayload jwtPayload) {
+    public boolean validateClaimNameIsNumeric(JwtPayload jwtPayload) {
         return !jwtPayload.getDetails().get("Name").toString().chars().anyMatch(Character::isDigit);
     }
-    private boolean validateClaimRoleNames(JwtPayload jwtPayload) {
+    public boolean validateClaimRoleNames(JwtPayload jwtPayload) {
         Collection<String> keys = Set.of("Admin", "Member", "External");
         return keys.contains(jwtPayload.getDetails().get("Role"));
     }
 
-    private boolean validadeSeedValue(JwtPayload jwtPayload) {
+    public boolean validadeSeedValue(JwtPayload jwtPayload) {
         return isPrimeNumber(Integer.parseInt(jwtPayload.getDetails().get("Seed").toString()));
     }
 
-    private boolean validadeMaxClaimNameChars(JwtPayload jwtPayload) {
+    public boolean validadeMaxClaimNameChars(JwtPayload jwtPayload) {
         return jwtPayload.getDetails().get("Name").length() <= 256;
     }
 
-    private boolean isPrimeNumber(int n)
+    public boolean isPrimeNumber(int n)
     {
         if (n <= 1)
             return false;
